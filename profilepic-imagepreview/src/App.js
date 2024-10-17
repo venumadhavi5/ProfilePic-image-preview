1e8311deadf47362f51dddccb3f0ecdf3fd773b2
venumadhavi5 <venumadhavi3350@gmail.com>
2024-10-17 23:10:58 +0530

    Add files via upload

---
+++ profilepic-imagepreview/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import SignupForm from './Components/SignupForm';
+
+function App() {
+  return (
+    <div className="App">
+      <SignupForm/>
+    </div>
+  );
+}
+
+export default App;
---
+++ profilepic-imagepreview/src/Components/SignupForm.js
@@ -0,0 +1,247 @@
+import React, {useRef, useState} from 'react'
+
+function SignupForm() {
+        let [profilePic,setProfilePic] =useState("./images/Dummy image.png");
+        let msgLabelRef= useRef();
+        let stateSelectRef=useRef();
+        let firstNameInputRef=useRef();
+        let lastNameInputRef=useRef();
+        let maleRBRef=useRef();
+        let femaleRBRef=useRef();
+        let selectedGender;
+        let salutation;
+        let maritalStatus;
+        let languagesKnown={
+            eng:false,
+            tel:false,
+            hin:false,
+            tam:false,
+            mal:false
+        }
+        let ageInputRef=useRef();
+    
+        let onClickSubmit =()=>{
+           if( selectedGender==="male"){
+            salutation ="Mr."
+           }else{
+            if(maritalStatus==="single"){
+            salutation="Miss."
+             } else{
+                salutation="Mrs."
+             } 
+             }
+    
+             console.log(languagesKnown);
+            msgLabelRef.current.innerHTML =`${salutation}${firstNameInputRef.current.value} ${lastNameInputRef.current.value} from ${stateSelectRef.current.value} ,your Account has been created successfully and you know
+             ${languagesKnown.tel===true?"Telugu":""} ${languagesKnown.eng===true?"English":""} ${languagesKnown.hin===true?"Hindi":""} ${languagesKnown.tam===true?"Tamil":""} ${languagesKnown.mal===true?"Malayalam":""} `
+        }
+  return (
+    <div>
+        <h1>Signup Form</h1>
+
+<form>
+    <div>
+        <label>FirstName</label>
+        <input ref={firstNameInputRef}></input>
+    </div>
+    <div>
+        <label>LastName</label>
+        <input ref={lastNameInputRef}></input>
+    </div>
+    <div>
+        <label>Gender</label>
+        <input type='radio' name='gender' ref={maleRBRef} onChange={()=>{
+               if(maleRBRef.current.checked===true){
+                selectedGender="male"
+
+               }
+        }}
+            ></input>
+        <label className='innerLabel'>Male</label>
+        <input type='radio' name='gender' ref={femaleRBRef} onChange={()=>{
+               if(femaleRBRef.current.checked===true){
+                selectedGender="female"
+
+               }
+        }}></input>
+        <label className='innerLabel'>Female</label>
+    </div>
+    <div>
+        <label>Marital Status</label>
+        <input type='radio' name='ms' onChange={(eventObj)=>{
+            console.log(eventObj);
+            if(eventObj.target.checked===true){
+                maritalStatus="single"
+
+            }
+        }
+
+    }></input>
+        <label className='innerLabel'>Single</label>
+        <input type='radio' name='ms'onChange={(eventObject)=>{
+            console.log(eventObject);
+            if(eventObject.target.checked===true){
+                maritalStatus="married"
+
+            }
+        }
+
+    }></input>
+        <label className='innerLabel'>Married</label>
+        
+    </div>
+    <div>
+        <label>State</label>
+        <select ref={stateSelectRef} onChange={()=>{
+            let state=stateSelectRef.current.value;
+            console.log(state);
+            switch (state) {
+                case "Andhra Pradesh":
+                    console.log("Amaravathi")
+                    
+                    break;
+            
+                
+                case "Telangana":
+                    console.log("Hyderabad")
+                    
+                    break;
+            
+                
+                case "Tamil Nadu":
+                    console.log("Chennai")
+                    
+                    break;
+            
+                
+                case "Kerala":
+                    console.log("Tiruvananthpuram")
+                    
+                    break;
+            
+               
+                case "Karnataka":
+                    console.log("Bengaluru")
+                    
+                    break;
+            
+                
+                case "Madhya Pradesh":
+                    console.log("Bhopal")
+                    
+                    break;
+            
+                
+                case "Gujarath":
+                    console.log("Gandhi Nagar")
+                    
+                    break;
+            
+                
+                case "Maharashtra":
+                    console.log("Mumbai")
+                    
+                    break;
+            
+                default:
+                    break;
+            }
+            
+        }}>
+            <option>Select State</option>
+            <option >Andhra Pradesh</option>
+            <option >Telangana</option>
+            <option >Tamil Nadu</option>
+            <option>Kerala</option>
+            <option >Karnataka</option>
+            <option >Madhya Pradesh</option>
+            <option >Gujarath</option>
+            <option >Maharashtra</option>
+            
+        </select>
+    </div>
+    <div>
+        <label>Languages Known</label>
+        <input type='checkbox' onChange={(eventObj)=>{
+           languagesKnown.tel= eventObj.target.checked;
+
+        }} ></input>
+        <label className='innerLabel'>Telugu</label>
+        <input type='checkbox' onChange={(eventObj)=>{
+           languagesKnown.eng= eventObj.target.checked;
+
+        }}  ></input>
+        <label className='innerLabel'>English</label>
+        <input type='checkbox' onChange={(eventObj)=>{
+           languagesKnown.hin= eventObj.target.checked;
+
+        }}  ></input>
+        <label className='innerLabel'>Hindi</label>
+        <input type='checkbox' onChange={(eventObj)=>{
+           languagesKnown.tam= eventObj.target.checked;
+
+        }}  ></input>
+        <label className='innerLabel'>Tamil</label>
+        <input type='checkbox'onChange={(eventObj)=>{
+           languagesKnown.mal= eventObj.target.checked;
+
+        }}   ></input>
+        <label className='innerLabel'>Malayalam</label>
+    </div>
+    <div>
+        <label>Email</label>
+        <input></input>
+    </div>
+    <div>
+        <label>Password</label>
+        <input></input>
+    </div>
+    <div>
+        <label>Age</label>
+        <input ref={ageInputRef} onChange={()=>{
+            let age= Number(ageInputRef.current.value);
+            if(age<5){
+                console.log(`Infant`)
+            }else if(age>=5 && age<=10){
+                console.log(`kid`)
+            }else if(age>=12 && age<=20){
+                console.log(`Teen`)
+            }else if(age>=20 && age<=30){
+                console.log(`Young`)
+               }   else if(age>=30 && age<=45){
+                    console.log(`Middle Age`)
+                }   else if(age>=45 && age<=70){
+                    console.log(`Old Age`)
+                }else{
+                    console.log(`not a valid value`)
+                }
+
+}}></input>
+    </div>
+    <div>
+        <label>Profile Pic</label>
+        <input type='file'  onChange={(eo)=>{
+         console.log(eo.target.files)
+         let selectedImagePath=URL.createObjectURL(eo.target.files[0]);
+         console.log(selectedImagePath)
+         setProfilePic(selectedImagePath)
+        }}></input>
+    </div>
+    <div>
+        <button type='button' onClick={()=>{
+            onClickSubmit();
+        }}>Submit</button>
+    </div>
+    <div>
+        <img src={profilePic} alt='' className='profilePic'></img>
+    </div>
+    <label ref={msgLabelRef} style={{width:"500px"}}></label>
+</form>
+      
+    </div>
+  )
+}
+      
+    
+
+export default SignupForm
